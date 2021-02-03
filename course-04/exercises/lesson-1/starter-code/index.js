37a3c32e43951da04b37749688323d4efcbc7b7e
raymond <raymondomon@yahoo.com>
2021-02-03 22:26:53 +0100

    modified function

--- course-04/exercises/lesson-1/starter-code/index.js
+++ course-04/exercises/lesson-1/starter-code/index.js
@@ -18,22 +18,22 @@ exports.handler = async (event) => {
   await axios.get(url)
 
   // Example of how to write a single data point
-  // await cloudwatch.putMetricData({
-  //   MetricData: [
-  //     {
-  //       MetricName: 'MetricName', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
-  //       Dimensions: [
-  //         {
-  //           Name: 'ServiceName',
-  //           Value: serviceName
-  //         }
-  //       ],
-  //       Unit: '', // 'Count' or 'Milliseconds'
-  //       Value: 0 // Total value
-  //     }
-  //   ],
-  //   Namespace: 'Udacity/Serveless'
-  // }).promise()
+  await cloudwatch.putMetricData({
+    MetricData: [
+      {
+        MetricName: 'MetricName', // Use different metric names for different values, e.g. 'Latency' and 'Successful'
+        Dimensions: [
+          {
+            Name: 'ServiceName',
+            Value: serviceName
+          }
+        ],
+        Unit: '', // 'Count' or 'Milliseconds'
+        Value: 0 // Total value
+      }
+    ],
+    Namespace: 'Udacity/Serveless'
+  }).promise()
 
   // TODO: Record time it took to get a response
   // TODO: Record if a response was successful or not
