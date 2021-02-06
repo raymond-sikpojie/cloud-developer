f245de996878b5d88b0d8908fe420390a1a2b879
raymond <raymondomon@yahoo.com>
2021-02-06 10:05:41 +0100

    modified apiEndpoin

--- course-04/exercises/lesson-2/client/src/config.ts
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -1,1 +1,2 @@
-export const apiEndpoint = '...'
+// export const apiEndpoint = 'https://yywrkta2ig.execute-api.us-east-1.amazonaws.com/dev/groups'
+export const apiEndpoint = 'https://yywrkta2ig.execute-api.us-east-1.amazonaws.com/dev'
